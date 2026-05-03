9c15b290a9f052bb8f880ee3893862acd7c77f1d
Qtoan297 <23004063@st.vlute.edu.vn>
2026-05-03 23:11:30 +0700

    Thêm ngành mới

--- app/controllers/nganh_controller.rb
+++ app/controllers/nganh_controller.rb
@@ -1,4 +1,34 @@
 class NganhController < ApplicationController
   def nganh
   end
+
+  def motanganh
+  end
+
+  def thuongmai
+  end
+
+  def logistics
+  end
+
+  def luat
+  end
+
+  def luat_kinh_te
+  end
+
+  def marketing
+  end
+
+  def truyen_thong_da_phuong_tien
+  end
+
+  def cong_nghe_thong_tin
+  end
+
+  def khoa_hoc_du_lieu
+  end
+
+  def toan_tin
+  end
 end
