bb7cdf170da3062ebc85bb73265317d02358c46b
ADuy <anhduylehuynh@gmail.com>
2026-05-24 18:11:06 +0700

    quan ly tai khoan v1

--- app/controllers/dangkytk_controller.rb
+++ app/controllers/dangkytk_controller.rb
@@ -1,22 +1,47 @@
 class DangkytkController < ApplicationController
   def dangkytk
   end
-  
-  
+
   def create
-    #tạo 1 đố tượng mới từ model Nguoidung với các thuộc tính được lấy từ params
+
+    # Mặc định là THISINH
+    vaitro = "THISINH"
+
+    # Nếu người tạo là ADMIN
+    if current_user && current_user.VAITRO == "ADMIN"
+      vaitro = "ADMIN"
+    end
+
+    # Tạo tài khoản mới
     nguoidung = Nguoidung.new(
       USERNAME: params[:tentk],
       EMAIL: params[:email],
-      MATKHAU: params[:matkhau]
+      MATKHAU: params[:matkhau],
+      VAITRO: vaitro
     )
-    #lưu đối tượng vào cơ sở dữ liệu và kiểm tra kết quả
+
+    # Lưu vào CSDL
     if nguoidung.save
-      redirect_to dangnhap_dangnhap_path,
-      notice: "Đăng ký thành công"
+
+      # Nếu ADMIN đang tạo tài khoản
+      if current_user && current_user.VAITRO == "ADMIN"
+
+        redirect_to qltaikhoan_qltaikhoan_path,
+        notice: "Thêm tài khoản thành công"
+
+      else
+
+        # Người dùng tự đăng ký
+        redirect_to dangnhap_dangnhap_path,
+        notice: "Đăng ký thành công"
+
+      end
+
     else
+
       flash[:alert] = "Đăng ký thất bại"
-      render :dangkytk #hiển thị lại form đăng ký
+      render :dangkytk
+
     end
   end
 end
--- app/controllers/qltaikhoan_controller.rb
+++ app/controllers/qltaikhoan_controller.rb
@@ -2,4 +2,39 @@ class QltaikhoanController < ApplicationController
   def qltaikhoan
     @nguoidungs = Nguoidung.all
   end
+
+  def delete
+    
+    user = Nguoidung.find(params[:id])
+
+    if user.IDNGUOIDUNG == current_user.IDNGUOIDUNG
+      redirect_to qltaikhoan_qltaikhoan_path,
+      alert: "Không thể xóa tài khoản đang đăng nhập"
+      return
+    end
+
+    user.destroy
+    redirect_to qltaikhoan_qltaikhoan_path,
+    notice: "Xóa tài khoản thành công"
+
+  end
+
+  def edit
+    @user = Nguoidung.find(params[:id])
+  end
+
+
+  def update
+
+      user = Nguoidung.find(params[:id])
+
+      user.update(
+          USERNAME: params[:USERNAME],
+          EMAIL: params[:EMAIL],
+          VAITRO: params[:VAITRO]
+      )
+
+      redirect_to qltaikhoan_qltaikhoan_path
+
+  end
 end
